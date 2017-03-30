16d4687712c272f545f37a2dcfd7a6dede980c94
LuckyXII <omnipotent117@gmail.com>
2017-03-30 10:23:00 +0200

    new file

--- inlmn5/index.js
+++ inlmn5/index.js
@@ -1,2 +1,19 @@
-/*jshint esnext: true, moz: true*/
-/*jslint browser:true */
+//REACT
+//=====================================================
+//GLOBALS
+
+var input1 = document.getElementById("textInput1");
+var output1 = document.getElementById("output1");
+//=====================================================
+//CLASSES
+class First extends React.Component{
+    render(){
+        return(
+             <p>HelloWorld</p>
+        );
+    }
+}
+
+//=====================================================
+//RENDER
+ReactDOM.render(<First />, output1 );
